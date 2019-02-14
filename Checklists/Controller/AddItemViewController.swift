052aef5303b4f66654cb40ae389144bbff8c3230
ThomasP <trolldu25@live.fr>
2019-02-14 14:09:51 +0100

    [FEAT] AddItemViewController

---
+++ Checklists/Controller/AddItemViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by lpiem on 14/02/2019.
+//  Copyright © 2019 lpiem. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+    @IBAction func Done(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+
+    }
+    
+    @IBAction func Cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
