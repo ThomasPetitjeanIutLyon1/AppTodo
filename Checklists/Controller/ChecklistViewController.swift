16c8d886a6544d4907e5e92249e7dee850153b9d
ThomasP <trolldu25@live.fr>
2019-02-14 10:52:30 +0100

    [ADD] ChecklistItem

--- Checklists/Controller/ChecklistViewController.swift
+++ Checklists/Controller/ChecklistViewController.swift
@@ -9,21 +9,28 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
+    
+    var list : [ChecklistItem]?
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        list = [ChecklistItem(text: "salut"),ChecklistItem(text: "sss", checked: true),ChecklistItem(text: "theo", checked: false)]
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return 1;
+        return (self.list?.count)!;
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath);
-        cell.textLabel?.text = "valeur fixe";
+        cell.textLabel?.text = list?[indexPath.row].text;
         return cell;
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true) }
 }
 
