46055ae3d50ca71d80f4857e04c0fed72bccdb7e
ThomasP <trolldu25@live.fr>
2019-02-14 09:57:22 +0100

    [FEAT] Cell Text

--- Checklists/ChecklistViewController.swift
+++ Checklists/ChecklistViewController.swift
@@ -15,6 +15,15 @@ class ChecklistViewController: UITableViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 1;
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath);
+        cell.textLabel?.text = "valeur fixe";
+        return cell;
+    }
 }
 
